88c3cb4e9c05b5ec5941576921fc4b9e97c32538
Monte Nolen <mortallylive@ya.ru>
2021-08-10 11:49:12 +0300

    lessons__

---
+++ python_repos.py
@@ -0,0 +1,15 @@
+
+import requests
+import json
+
+# Создание вызова API и сохранение ответа.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Сохранение ответа API в переменной.
+response_dict = r.json()
+
+# Обработка результатов.
+print(response_dict.keys())
